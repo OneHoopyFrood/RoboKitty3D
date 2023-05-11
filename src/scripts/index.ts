a68f4f3c44d150e6ff9dec56fbbce0a5eedb2921
Cole <cole.panike@janus-ai.com>
2023-05-11 11:15:10 -0500

    ✨ Add Symbols to cubes!

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,19 +1,21 @@
-import * as THREE from "three"
-import { WebGLRenderer } from "three"
-import { makeCube } from "./makeCube"
-import { setupPlayerMovement, updateCamera } from "./playerMovement"
+import * as THREE from 'three'
+import { WebGLRenderer } from 'three'
+import { AllowedSymbols, SYMBOLS, makeCube } from './makeCube'
+import { setupPlayerMovement, updateCamera } from './playerMovement'
 
-import "../styles/index.css"
+import '../styles/index.css'
 
 const WIDTH = window.innerWidth
 const HEIGHT = window.innerHeight
 
 const CUBE_SIZE = 10
+const GRID_SIZE = 500
 
 function setupRenderer() {
   const renderer = new THREE.WebGLRenderer({ antialias: true })
   renderer.setSize(WIDTH, HEIGHT)
   renderer.setClearColor(0x222222, 1)
+  renderer.domElement.id = 'game'
   document.body.appendChild(renderer.domElement)
   renderer.setPixelRatio(window.devicePixelRatio)
   renderer.setSize(window.innerWidth, window.innerHeight)
@@ -33,14 +35,14 @@ function setupCamera(renderer: WebGLRenderer) {
     camera.updateProjectionMatrix()
     renderer.setSize(window.innerWidth, window.innerHeight)
   }
-  window.addEventListener("resize", onWindowResize, false)
+  window.addEventListener('resize', onWindowResize, false)
 
   return camera
 }
 
 function setupCrosshair() {
-  const crosshair = document.createElement("div")
-  crosshair.classList.add("crosshair")
+  const crosshair = document.createElement('div')
+  crosshair.classList.add('crosshair')
   document.body.appendChild(crosshair)
 }
 
@@ -48,6 +50,9 @@ function setup(): [WebGLRenderer, THREE.Scene, THREE.Camera] {
   const renderer = setupRenderer()
 
   const scene = new THREE.Scene()
+
+  const ambientLight = new THREE.AmbientLight(0xffffff) // white light
+  scene.add(ambientLight)
 
   const camera = setupCamera(renderer)
 
@@ -57,9 +62,13 @@ function setup(): [WebGLRenderer, THREE.Scene, THREE.Camera] {
   const gridHelper = new THREE.GridHelper(500, 100)
   scene.add(gridHelper)
 
-  return [renderer, scene, camera ]
+  return [renderer, scene, camera]
 }
 
+/**
+ * Generates a random THREE.Color
+ * @returns A random color
+ */
 function genRandomColor() {
   const r = Math.random()
   const g = Math.random()
@@ -67,22 +76,46 @@ function genRandomColor() {
   return new THREE.Color(r, g, b)
 }
 
+/**
+ * Generates a random position that will fall within the grid
+ * @returns A random position between the grid bounds
+ */
+function genRandomPosition(mapSize: number, aproxObjectDiameter: number): number {
+  const halfMapSize = mapSize / 2
+  const lowerBound = -halfMapSize + aproxObjectDiameter / 2
+  const upperBound = halfMapSize - aproxObjectDiameter / 2
+  return Math.random() * (upperBound - lowerBound) + lowerBound
+}
+
+function genRandomSymbol(): AllowedSymbols {
+  const randomIndex = Math.floor(Math.random() * SYMBOLS.length)
+  return SYMBOLS[randomIndex]
+}
+
 function generateCubes(numCubes = 100) {
   const cubes: THREE.Mesh<THREE.BoxGeometry, THREE.MeshBasicMaterial>[] = []
-  while (cubes.length < numCubes) {
+  // I'm using a for loop here because I want to be able to limit the number or
+  // retries to something reasonable. Normally you'd use a while loop here, but
+  // that TECHNICALLY has the potential to run forever.
+  for (let i = 0; cubes.length < numCubes; i++) {
+    if (i > numCubes * 3) {
+      console.warn(`Could not generate enough cubes! Only ${cubes.length} cubes generated.`)
+      break
+    }
+
     const cube = makeCube(
       CUBE_SIZE,
       genRandomColor(),
-      Math.random() * 500 - 5 - 250,
-      Math.random() * 500 - 5 - 250
+      // Generate a random symbol within the available symbols
+      genRandomSymbol(),
+      genRandomPosition(GRID_SIZE, CUBE_SIZE),
+      genRandomPosition(GRID_SIZE, CUBE_SIZE),
     )
 
     // Prevent cubes from spawning inside each other
-    if (cubes.some((c) => c.position.distanceTo(cube.position) < CUBE_SIZE * 2 )) {
-      continue
+    if (!cubes.some((c) => c.position.distanceTo(cube.position) < CUBE_SIZE * 2)) {
+      cubes.push(cube)
     }
-    
-    cubes.push(cube)
   }
   return cubes
 }
@@ -94,7 +127,7 @@ function main() {
   const cameraControls = setupPlayerMovement(camera, renderer.domElement)
 
   setupCrosshair()
-  
+
   // Populate some cubes, yo
   const cubes = generateCubes(30)
   cubes.forEach((cube) => scene.add(cube))
--- src/scripts/makeCube.ts
+++ src/scripts/makeCube.ts
@@ -1,11 +1,121 @@
-import * as THREE from "three"
+import * as THREE from 'three'
 
-export function makeCube(size: number, color: THREE.Color, x = 0, z = 0, y = 0) {
-  const cubeGeometry = new THREE.BoxGeometry(size, size, size) // Create a cube geometry with dimensions of 1x1x1 units
-  const cubeMaterial = new THREE.MeshBasicMaterial({
-    color,
-  }) // Set the material to be red
-  const cube = new THREE.Mesh(cubeGeometry, cubeMaterial) // Create a mesh using the cube geometry and material
+// Create one canvas and reuse it for each symbol
+const canvas = document.createElement('canvas')
+const SYMBOL_CANVAS_SIZE = 128 // Make sure this is a power of two
+canvas.width = SYMBOL_CANVAS_SIZE
+canvas.height = SYMBOL_CANVAS_SIZE
+const ctx = canvas.getContext('2d')
+
+// prettier-ignore
+export const SYMBOLS: [ '!', '#', '$', '%', '&', '*', '+', '.', '/', '0', '3', '8', ';', '=', 
+                        '?', 'A', 'F', 'X', 'a', 'f', 'g', 'h', 'i', 'm', 'n', 't', 'w', ] = 
+                      [ '!', '#', '$', '%', '&', '*', '+', '.', '/', '0', '3', '8', ';', '=', 
+                        '?', 'A', 'F', 'X', 'a', 'f', 'g', 'h', 'i', 'm', 'n', 't', 'w', ]
+
+export type AllowedSymbols = (typeof SYMBOLS)[number]
+type HexColorString = `#${string}`
+
+function isHexColorString(str: string): str is HexColorString {
+  return str.length === 4 || (str.length === 7 && str[0] === '#')
+}
+
+if (ctx === null) {
+  throw new Error('Could not get canvas context for symbol texture')
+}
+
+function createSymbolTexture(
+  symbol: AllowedSymbols,
+  fontSize: number,
+  fontColor: HexColorString,
+  backgroundColor: HexColorString,
+) {
+  validateSymbolGenArgs(symbol, fontSize, fontColor)
+
+  if (ctx === null) {
+    throw new Error('Could not get canvas context for symbol texture')
+  }
+
+  // Clear canvas before drawing new symbol
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+  // Fill the canvas with a solid color
+  ctx.fillStyle = backgroundColor // Set the background color
+  ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+  // Draw symbol
+  ctx.font = `bold ${fontSize}px Terminus, monospace, Arial, sans-serif`
+  ctx.fillStyle = fontColor
+  ctx.textAlign = 'center'
+  ctx.textBaseline = 'middle'
+  ctx.fillText(symbol, canvas.width / 2, canvas.height / 2)
+
+  // Create an image from the canvas so that the texture won't follow the state
+  // of the canvas that we're reusing
+  const image = new Image()
+  image.src = canvas.toDataURL()
+
+  // Create a texture from the image
+  const texture = new THREE.Texture(image)
+  texture.needsUpdate = true
+
+  // Enable mipmapping
+  texture.generateMipmaps = true
+  texture.minFilter = THREE.LinearMipmapLinearFilter
+
+  return texture
+}
+
+function validateSymbolGenArgs(symbol: string, fontSize: number, fontColor: string) {
+  if (symbol.length !== 1) {
+    throw new Error('Symbol must be a single character')
+  }
+  if (fontSize > SYMBOL_CANVAS_SIZE) {
+    throw new Error('Font size must be less than the canvas size')
+  }
+  if (fontSize <= 0) {
+    throw new Error('Font size must be greater than 0')
+  }
+  if (fontSize % 1 !== 0) {
+    throw new Error('Font size must be an integer')
+  }
+  if ((fontColor.length !== 4 && fontColor.length !== 7) || fontColor[0] !== '#') {
+    throw new Error('Font color must be a hex color string')
+  }
+}
+
+function genSymbolMaterial(
+  symbol: AllowedSymbols,
+  fontColor: HexColorString,
+  bgColor: HexColorString,
+): THREE.MeshBasicMaterial {
+  const texture = createSymbolTexture(symbol, 64, fontColor, bgColor)
+  const material = new THREE.MeshBasicMaterial({ map: texture })
+  return material
+}
+
+export function makeCube(size: number, color: THREE.Color, symbol: AllowedSymbols, x = 0, z = 0, y = 0) {
+  const cubeGeometry = new THREE.BoxGeometry(size, size, size)
+
+  const backgroundColor: HexColorString = `#${color.getHexString()}`
+  if (!isHexColorString(backgroundColor)) {
+    throw new Error('Could not convert background color to hex string')
+  }
+
+  // The font color should be the same as the background color, but offset in
+  // brightness either up or down depending on the brightness of the background
+  const target = { h: 0, s: 0, l: 0 }
+  const fontColorLightnessOffset = 0.3
+  const fontColorLightness =
+    color.getHSL(target).l > 0.5 ? target.l - fontColorLightnessOffset : target.l + fontColorLightnessOffset
+  const fontColor = `#${new THREE.Color().setHSL(target.h, target.s, fontColorLightness).getHexString()}`
+  if (!isHexColorString(fontColor)) {
+    throw new Error('Could not convert font color to hex string')
+  }
+
+  const symbolMaterial = genSymbolMaterial(symbol, fontColor, backgroundColor)
+
+  const cube = new THREE.Mesh(cubeGeometry, symbolMaterial)
   cube.position.z = z
   cube.position.x = x
   cube.position.y = y || size / 2
