db2b8da79635c5a41ad1ba382f9e55a7c26751bf
Ivan <spugintrntl@gmail.com>
2023-05-16 21:03:50 -0400

    commented out code in audio.ts so program will run

--- src/assets/audio.ts
+++ src/assets/audio.ts
@@ -2,17 +2,17 @@
 // find out how to integrate it and figure out how things are going to work.  
 
 // create an AudioListener and add it to the camera
-const listener = new THREE.AudioListener();
-camera.add( listener );
+//const listener = new THREE.AudioListener();
+//camera.add( listener );
 
 // create a global audio source
-const sound = new THREE.Audio( listener );
+//const sound = new THREE.Audio( listener );
 
 // load a sound and set it as the Audio object's buffer
-const audioLoader = new THREE.AudioLoader();
-audioLoader.load( 'Nostalgium BGM.ogg', function( buffer ) {
-	sound.setBuffer( buffer );
-	sound.setLoop( true );
-	sound.setVolume( 0.5 );
-	sound.play();
-});
+//const audioLoader = new THREE.AudioLoader();
+//audioLoader.load( 'Nostalgium BGM.ogg', function( buffer ) {
+//	sound.setBuffer( buffer );
+//	sound.setLoop( true );
+//	sound.setVolume( 0.5 );
+//	sound.play();
+//});
