1f0827fba80f63248cfd3272b9b4e43ad4ebb602
Ivan <spugintrntl@gmail.com>
2023-05-27 00:14:03 -0400

    Softer moosic

--- src/scripts/Audio.ts
+++ src/scripts/Audio.ts
@@ -1,18 +1,24 @@
-// I just copied some example code into a new script file so hopefull I can play around with it in our next session,
-// find out how to integrate it and figure out how things are going to work.  
+/**
+ * @fileoverview
+ * This file contains the audio setup for the game. (SFX and BGM)
+ */
 
-// create an AudioListener and add it to the camera
-const listener = new THREE.AudioListener();
-camera.add( listener );
+import { Audio, AudioListener, AudioLoader, Camera } from 'three'
 
-// create a global audio source
-const sound = new THREE.Audio( listener );
+export function setupAudio(camera: Camera) {
+  // create an AudioListener and add it to the camera
+  const listener = new AudioListener()
+  camera.add(listener)
 
-// load a sound and set it as the Audio object's buffer
-const audioLoader = new THREE.AudioLoader();
-audioLoader.load( 'Nostalgium BGM.ogg', function( buffer ) {
-	sound.setBuffer( buffer );
-	sound.setLoop( true );
-	sound.setVolume( 0.5 );
-	sound.play();
-});
+  // create a global audio source
+  const sound = new Audio(listener)
+
+  // load a sound and set it as the Audio object's buffer
+  const audioLoader = new AudioLoader()
+  audioLoader.load('assets/Nostalgium 2023.ogg', function (buffer) {
+    sound.setBuffer(buffer)
+    sound.setLoop(true)
+    sound.setVolume(0.5)
+    sound.play()
+  }) 
+}
