b04c765cd624d03e86801c82e7bf32822cf42e19
Cole <OneHoopyFrood@users.noreply.github.com>
2025-05-23 19:22:14 -0500

    Remove some more c#, refining structure

---
+++ Core/Game.gd
@@ -0,0 +1,49 @@
+extends Node3D
+
+@export var num_cubes: int = 100
+@export var spawn_radius: float = 50.0
+
+var rng: RandomNumberGenerator = RandomNumberGenerator.new()
+
+var cube_scene = preload('res://World/Cube/Cube.tscn')
+
+func _ready():
+  rng.randomize()
+  add_child(_create_ground_plane())
+
+  # Cubes
+  var cubes = _generate_cubes()
+  for cube in cubes:
+    add_child(cube)
+
+func _generate_cubes() -> Array:
+  var cubes: Array[Cube];
+  for i in range(num_cubes):
+    var cube: Cube = cube_scene.instantiate() as Cube
+
+    cube.randomize_bobbing(rng)
+
+    cube.position = Vector3(
+      rng.randf_range(-spawn_radius, spawn_radius),
+      0.5,
+      rng.randf_range(-spawn_radius, spawn_radius)
+    )
+
+    cubes.append(cube)
+  return cubes
+
+func _create_ground_plane() -> MeshInstance3D:
+  var ground = MeshInstance3D.new()
+
+  var plane = PlaneMesh.new()
+  plane.size = Vector2(spawn_radius * 2, spawn_radius * 2)
+  plane.subdivide_width = 20
+  plane.subdivide_depth = 20
+  ground.mesh = plane
+
+  var shader = load("res://Shaders/InfiniteGrid.gdshader")
+  var shader_mat = ShaderMaterial.new()
+  shader_mat.shader = shader
+  ground.set_surface_override_material(0, shader_mat)
+
+  return ground
---
+++ World/Cube/Cube.gd
@@ -0,0 +1,54 @@
+# Cube.gd
+# Script attached to Cube.tscn – handles floating/bobbing animation.
+
+class_name Cube
+extends Node3D
+
+########################
+## CONSTANTS
+########################
+# Clamp values
+const MIN_AMPLITUDE: float = 0.1
+const MAX_AMPLITUDE: float = 0.5
+const MIN_SPEED: float = 0.5
+const MAX_SPEED: float = 2.0
+const MAX_Y: float = 2.0
+
+# Defaults
+const DEFAULT_AMPLITUDE: float = 0.25
+const DEFAULT_SPEED: float = 1.0
+const DEFAULT_Y: float = 1.0
+
+########################
+## Active values
+########################
+var bob_amplitude: float = DEFAULT_AMPLITUDE
+var bob_speed: float = DEFAULT_SPEED
+var base_y: float # Height to float (centerpoint of box)
+
+# Private
+var _time: float = 0.0
+@onready
+var _mesh := get_node("Mesh") as MeshInstance3D
+
+func _ready():
+  var box_size = _mesh.get_aabb().size
+  # Make sure the box isn't in the ground or too high.
+  base_y = clamp(global_position.y, box_size.y / 2, MAX_Y)
+
+func _process(delta: float) -> void:
+  _time += delta
+  var offset = sin(_time * bob_speed) * bob_amplitude
+  global_position.y = base_y + offset
+
+# External configuration
+func configure_bobbing(
+  amplitude: float,
+  speed: float
+) -> void:
+  bob_amplitude = clamp(amplitude, MIN_AMPLITUDE, MAX_AMPLITUDE)
+  bob_speed = clamp(speed, MIN_SPEED, MAX_SPEED)
+
+func randomize_bobbing(rng: RandomNumberGenerator):
+  bob_amplitude = rng.randf_range(MIN_AMPLITUDE, MAX_AMPLITUDE)
+  bob_speed = rng.randf_range(MIN_SPEED, MAX_SPEED)
