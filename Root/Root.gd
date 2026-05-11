8c7f191b77f159000a2760312ed11961119d4c8f
Cole <onehoopyfrood@users.noreply.github.com>
2026-05-11 09:38:07 -0500

    Refactor cheats to encapsulate

--- Root/Root.gd
+++ Root/Root.gd
@@ -6,9 +6,8 @@ class_name Root
 @onready var _world = $World
 @onready var _player = $World/Player
 @onready var _dialog = $Dialog
+@onready var _cheat_console = $CheatConsoleLayer
 @onready var _music: AudioStreamPlayer = $BackgroundMusic
-@onready var _cheat_panel: PanelContainer = $CheatConsoleLayer/CheatPanel
-@onready var _cheat_label: Label = $CheatConsoleLayer/CheatPanel/CheatLabel
 
 const _MUSIC_TRACKS: Array[AudioStream] = [
   preload("res://Assets/music/Nostalgium 2023.ogg"),
@@ -21,8 +20,6 @@ const _WIN_RESTART_PROMPT: String = "Press any key to restart"
 var _current_scene: String = "menu" # "menu" or "world"
 var _has_won: bool = false
 var _track_index: int = 0
-var _is_cheat_prompt_open: bool = false
-var _cheat_input_buffer: String = ""
 
 
 func _ready() -> void:
@@ -38,9 +35,10 @@ func _ready() -> void:
   if _world.has_signal("kitten_found") and not _world.kitten_found.is_connected(_on_kitten_found):
     _world.kitten_found.connect(_on_kitten_found)
 
+  if not _cheat_console.cheat_activated.is_connected(_on_cheat_activated):
+    _cheat_console.cheat_activated.connect(_on_cheat_activated)
+
   _menu.button_pressed.connect(_on_menu_button_pressed)
-  _cheat_panel.visible = false
-  _update_cheat_prompt_label()
 
   _hide_menu()
 
@@ -100,12 +98,12 @@ func _unhandled_input(event: InputEvent) -> void:
 
 
 func _handle_root_input(event: InputEvent) -> bool:
-  if _is_cheat_prompt_open:
-    _handle_cheat_prompt_input(event)
+  if _cheat_console.is_cheat_prompt_open():
+    _cheat_console.handle_cheat_prompt_input(event)
     return true
 
-  if _current_scene == "menu" and _is_cheat_prompt_toggle_input(event):
-    _open_cheat_prompt()
+  if _current_scene == "menu" and _cheat_console.is_cheat_prompt_toggle_input(event):
+    _cheat_console.open_cheat_prompt()
     return true
 
   if _has_won and event is InputEventKey and event.pressed and not event.echo:
@@ -125,7 +123,7 @@ func _restart_after_win() -> void:
 
 func _show_menu() -> void:
   _current_scene = "menu"
-  _close_cheat_prompt(false)
+  _cheat_console.close_cheat_prompt(false)
 
   # Disable player controls and hide world, but keep them active so they can be seen in the background.
   _player.disable_controls()
@@ -146,90 +144,12 @@ func _show_menu() -> void:
 
 func _hide_menu() -> void:
   _current_scene = "world"
-  _close_cheat_prompt(false)
+  _cheat_console.close_cheat_prompt(false)
   _menu.visible = false
   _player.enable_controls()
   _world.visible = true
   _world.process_mode = Node.PROCESS_MODE_INHERIT
   Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-
-
-func _is_cheat_prompt_toggle_input(event: InputEvent) -> bool:
-  if not (event is InputEventKey):
-    return false
-
-  var key_event := event as InputEventKey
-  if not key_event.pressed or key_event.echo:
-    return false
-
-  if key_event.ctrl_pressed or key_event.alt_pressed or key_event.meta_pressed:
-    return false
-
-  if key_event.keycode == KEY_QUOTELEFT:
-    return true
-
-  if key_event.physical_keycode == KEY_QUOTELEFT:
-    return true
-
-  return key_event.unicode == 96
-
-
-func _open_cheat_prompt() -> void:
-  _is_cheat_prompt_open = true
-  _cheat_input_buffer = ""
-  _cheat_panel.visible = true
-  _update_cheat_prompt_label()
-
-
-func _close_cheat_prompt(should_execute: bool) -> void:
-  if should_execute:
-    _execute_cheat_code(_cheat_input_buffer)
-
-  _is_cheat_prompt_open = false
-  _cheat_input_buffer = ""
-  _cheat_panel.visible = false
-  _update_cheat_prompt_label()
-
-
-func _handle_cheat_prompt_input(event: InputEvent) -> void:
-  if not (event is InputEventKey):
-    return
-
-  var key_event := event as InputEventKey
-  if not key_event.pressed or key_event.echo:
-    return
-
-  if key_event.keycode == KEY_ESCAPE:
-    _close_cheat_prompt(false)
-    return
-
-  if key_event.keycode == KEY_ENTER or key_event.keycode == KEY_KP_ENTER:
-    _close_cheat_prompt(true)
-    return
-
-  if key_event.keycode == KEY_BACKSPACE:
-    if not _cheat_input_buffer.is_empty():
-      _cheat_input_buffer = _cheat_input_buffer.substr(0, _cheat_input_buffer.length() - 1)
-      _update_cheat_prompt_label()
-    return
-
-  if key_event.ctrl_pressed or key_event.alt_pressed or key_event.meta_pressed:
-    return
-
-  if key_event.unicode >= 32:
-    _cheat_input_buffer += char(key_event.unicode)
-    _update_cheat_prompt_label()
-
-
-func _update_cheat_prompt_label() -> void:
-  _cheat_label.text = "> %s" % _cheat_input_buffer
-
-
-func _execute_cheat_code(input_code: String) -> void:
-  var normalized_code := input_code.strip_edges().to_lower()
-  if normalized_code == "mrrow":
-    resume()
-    _world.bump_kitten()
 
 
 func resume() -> void:
@@ -290,3 +210,9 @@ func _on_kitten_found() -> void:
   _player.disable_controls()
   if _dialog and _dialog.has_method("open"):
     _dialog.open("", _WIN_RESTART_PROMPT)
+
+
+func _on_cheat_activated(code: String) -> void:
+  if code == "mrrow":
+    resume()
+    _world.bump_kitten()
